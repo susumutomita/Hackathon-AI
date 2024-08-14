434b26ad22a97c357bb39bf71a3c607fb1ee5678
susumu tomita <oyster880@gmail.com>
2024-08-15 07:37:44 +0900

    Initial Commit

---
+++ HackathonAI/HackathonAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonAIApp.swift
+//  HackathonAI
+//
+//  Created by susumu tomita on 2024/08/15.
+//
+
+import SwiftUI
+
+@main
+struct HackathonAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
