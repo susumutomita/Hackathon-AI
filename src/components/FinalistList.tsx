bcc69c009b015f6abb2d912f7ff4e544027acfe1
susumu tomita <oyster880@gmail.com>
2024-08-22 21:23:42 +0900

    update list

--- src/components/FinalistList.tsx
+++ src/components/FinalistList.tsx
@@ -85,7 +85,7 @@ export default function FinalistList() {
             </TableBody>
           </Table>
         ) : (
-          !loading && <p>No projects found.</p>
+          !loading
         )}
       </div>
     </div>
